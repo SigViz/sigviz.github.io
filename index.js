b53b691502a58eeb3439a6cd1eb53a12a8e523e6
ZachCraw <ZachCraw@users.noreply.github.com>
2025-09-04 22:38:10 +0000

    deploy: SigViz/SigViz@38df0ce0b65c741fffd9b568226fe8f32af34973

--- index.js
+++ index.js
@@ -191,7 +191,7 @@ Module['FS_createPath']('/', 'assets', true, true);
   }
 
  }
- loadPackage({"files": [{"start": 0, "audio": 0, "end": 2374264, "filename": "/assets/JetBrainsMonoNLNerdFont-Regular.ttf"}], "remote_package_size": 2374264, "package_uuid": "42b3d7c2-4563-4b60-8e7c-6e0df75577b1"});
+ loadPackage({"files": [{"start": 0, "audio": 0, "end": 3030, "filename": "/assets/Logo.png"}, {"start": 3030, "audio": 0, "end": 2377294, "filename": "/assets/JetBrainsMonoNLNerdFont-Regular.ttf"}], "remote_package_size": 2377294, "package_uuid": "6d25d049-f3e7-4237-b3d2-b010c9743bea"});
 
 })();
 
