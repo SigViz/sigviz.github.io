4642f82d3c91caecbfa656ef0bbfd9be4ca813ca
ZachCraw <ZachCraw@users.noreply.github.com>
2025-09-07 23:20:49 +0000

    deploy: SigViz/SigViz@4c87580a866365054e69787cc6acbabe2855d5a1

--- index.js
+++ index.js
@@ -191,7 +191,7 @@ Module['FS_createPath']('/', 'assets', true, true);
   }
 
  }
- loadPackage({"files": [{"start": 0, "audio": 0, "end": 3030, "filename": "/assets/Logo.png"}, {"start": 3030, "audio": 0, "end": 2377294, "filename": "/assets/JetBrainsMonoNLNerdFont-Regular.ttf"}], "remote_package_size": 2377294, "package_uuid": "b25a2ddb-68f3-47a4-9e92-cb18e0cc5bb9"});
+ loadPackage({"files": [{"start": 0, "audio": 0, "end": 3030, "filename": "/assets/Logo.png"}, {"start": 3030, "audio": 0, "end": 2377294, "filename": "/assets/JetBrainsMonoNLNerdFont-Regular.ttf"}], "remote_package_size": 2377294, "package_uuid": "2e50d4ca-a1a6-4d0d-98fb-f9f33456b3e5"});
 
 })();
 
@@ -1396,11 +1396,11 @@ function updateGlobalBufferAndViews(buf) {
 
 
 var STATIC_BASE = 1024,
-    STACK_BASE = 155152,
+    STACK_BASE = 155312,
     STACKTOP = STACK_BASE,
-    STACK_MAX = 5398032,
-    DYNAMIC_BASE = 5398032,
-    DYNAMICTOP_PTR = 155120;
+    STACK_MAX = 5398192,
+    DYNAMIC_BASE = 5398192,
+    DYNAMICTOP_PTR = 155280;
 
 assert(STACK_BASE % 16 === 0, 'stack must start aligned');
 assert(DYNAMIC_BASE % 16 === 0, 'heap must start aligned');
@@ -1949,7 +1949,7 @@ function _emscripten_asm_const_iiii(code, a0, a1, a2) {
 
 
 
-// STATICTOP = STATIC_BASE + 154128;
+// STATICTOP = STATIC_BASE + 154288;
 /* global initializers */  __ATINIT__.push({ func: function() { ___emscripten_environ_constructor() } });
 
 
@@ -1960,7 +1960,7 @@ function _emscripten_asm_const_iiii(code, a0, a1, a2) {
 
 
 /* no memory initializer */
-var tempDoublePtr = 155136
+var tempDoublePtr = 155296
 assert(tempDoublePtr % 8 == 0);
 
 function copyTempFloat(ptr) { // functions, because inlining this code increases code size too much
